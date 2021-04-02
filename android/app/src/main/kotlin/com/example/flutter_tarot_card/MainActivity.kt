c10dba7264d7ef7868c62d3c235346c08e3df602
lambiengcode <lambiengcode@gmail.com>
2021-04-02 22:03:04 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_tarot_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_tarot_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(GetMaterialApp(
+    
+  ),);
+}
